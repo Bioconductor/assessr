6d99d09c4fd700b46be54aa01419079ffbe031a3
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2020-03-07 14:27:06 -0500

    Recode to work with BioC2020 submission

--- app.R
+++ app.R
@@ -3,57 +3,64 @@ library(shinydashboard)
 library(clipr)
 library(rintrojs)
 
+# URL(s) to Google Form(s) for Evaluation 
+# workshop_form <- function(id) {
+#   paste0("https://docs.google.com/forms/d/e/1FAIpQLScTMLTJ1ccfBmjdEPhZfk5CyQwqSAW5AUJyDkFxc7Q9ZW6VPQ/viewform?usp=pp_url&entry.840333480=",
+#          id)
+# }
+# contribution_form <- function(id) {
+#   paste0("https://docs.google.com/forms/d/e/1FAIpQLSezGbJ1JmgOwDI5BLl28gXp3YQfoFXq8GoMon3k9PZcePCF_w/viewform?usp=pp_url&entry.840333480=",
+#          id)
+# }
 
+# To pre-populate an element in the evaluation form, drill down to this element
+# using Chrome developer tools and get element id as, e.g., name="entry.1845598857"
 workshop_form <- function(id) {
-  paste0("https://docs.google.com/forms/d/e/1FAIpQLScTMLTJ1ccfBmjdEPhZfk5CyQwqSAW5AUJyDkFxc7Q9ZW6VPQ/viewform?usp=pp_url&entry.840333480=",
-         id)
+  paste0("https://docs.google.com/forms/d/e/1FAIpQLSc4iDxze_Xm8cnYuVuMDnR6rVI7kj7gjL8f5ow4hd2zDJk-mw/viewform?usp=pp_url&entry.1845598857=", id)
 }
 
-contribution_form <- function(id) {
-  paste0("https://docs.google.com/forms/d/e/1FAIpQLSezGbJ1JmgOwDI5BLl28gXp3YQfoFXq8GoMon3k9PZcePCF_w/viewform?usp=pp_url&entry.840333480=",
-         id)
-}
+# Open link in a new window
 window_open <- function(form_link) {
   paste0("window.open('", form_link, "', '_blank')")
 }
-
-
-window_open_erum <- function(id, type) {
+# Open link to evaluation table for a given type of evaluation, if multiple
+window_open_eval <- function(id, type) {
   if (type == "workshop") {
     link <- workshop_form(id)
   } else if (type == "contribution") {
     link <- contribution_form(id)
   }
-  
   window_open(link)
 }
 
 
+# Excel file(s) with session information
+# workshop_path     <- "./Bioc2020 talks and posters review.xlsx"
+# contribution_path <- "./BioC2020_talks_posters.xlsx"
+# 
+# workshop_table <- readxl::read_excel(workshop_path)
+# contribution_table_raw <- readxl::read_excel(contribution_path)
+# contribution_table <- contribution_table_raw[!is.na(contribution_table_raw$Id),]
 
-  workshop_path     <- "./BioC2020_workshops.xlsx"
-  contribution_path <- "./BioC2020_talks_posters.xlsx"
-  
-  workshop_table <- readxl::read_excel(workshop_path)
-  contribution_table_raw <- readxl::read_excel(contribution_path)
-  contribution_table <- contribution_table_raw[!is.na(contribution_table_raw$Id),]
-  
-  
-  reviewers_names <- unique(c(workshop_table$Reviewer1, workshop_table$Reviewer2,
-                              workshop_table$Reviewer3))
-  
-  preselected_cols <- c("Id",
-                        "Title",
-                        # "Description",
-                        "Session format",
-                        "Track",
-                        "Keywords (1-3)",
-                        "Link",
-                        "First time presenting this?"
-                        # "Speaker Notes"
-                        )
-  
-  abstract_table_compact <- 
-    workshop_table[, preselected_cols]
+# Alternatively, read directly from a GSheet that has been shared for view 
+library(gsheet) # install.packages("gsheet")
+workshop_table <- gsheet2tbl("https://docs.google.com/spreadsheets/d/1kaYuJKlt7sNSiLumQYnZau5TgTx9W9SkSQsQa8xBCLM/edit?usp=sharing")
+# Remove duplicate submissions, ignoring Timestamp
+workshop_table <- workshop_table[!duplicated(workshop_table[, colnames(workshop_table) != "Timestamp"]), ]
+
+# Get unique reviewer's names
+reviewers_names <- workshop_table %>% dplyr::select(starts_with("Reviewer")) %>% unlist %>% unique() 
+reviewers_names <- reviewers_names[!is.na(reviewers_names)] # Remove NA reviwers
+
+# Currently, all columns need to be manually recoded
+# colnames(workshop_table) 
+# Which columns to display
+preselected_cols <- c("Name of presenter",
+                      "Affiliation of presenter",
+                      "Title of talk / poster",
+                      "What are you proposing to present?")
+# Compact table to display
+abstract_table_compact <- workshop_table[, preselected_cols]
   
   # UI definition -----------------------------------------------------------
   assessr_ui <- shinydashboard::dashboardPage(
@@ -74,7 +81,8 @@ window_open_erum <- function(id, type) {
         selectInput(
           inputId = "submissionType",
           label = "Type of contribution",
-          choices = c("Workshops" = "workshop", "Talks and other sessions" = "contribution"),
+          # choices = c("Workshops" = "workshop", "Talks and other sessions" = "contribution"),
+          choices = c("Workshops" = "workshop"),
           selected =  "Workshop", 
           multiple = FALSE
         ),
@@ -92,7 +100,6 @@ window_open_erum <- function(id, type) {
           selected =  "All", 
           multiple = FALSE
         ),
-        
         
         actionButton(
           inputId = "tour_assessr",
@@ -121,13 +128,9 @@ window_open_erum <- function(id, type) {
       )
     )
   )
-  
-  
+
   # Server definition -------------------------------------------------------
   assessr_server <- function(input, output, session) {
-    
-    
-    
     abstract_table <- reactive({
       message("input$submissionType: ", input$submissionType)
       if (input$submissionType == "workshop") {
@@ -135,17 +138,26 @@ window_open_erum <- function(id, type) {
       } else if (input$submissionType == "contribution") {
         abstract_t <- contribution_table
       }
-      
       abstract_t
     })
     
     current_dt <- reactive({
       message("input$reviewer: ", input$reviewer)
-      mydt <- abstract_table()[abstract_table()$Reviewer1 %in% input$reviewer |
-                                 abstract_table()$Reviewer2 %in% input$reviewer |
-                                 abstract_table()$Reviewer3 %in% input$reviewer |
+      # ToDo: Automate reviewers column selection
+      mydt <- abstract_table()[abstract_table()$`Reviewer 1` %in% input$reviewer |
+                                 abstract_table()$`Reviewer 2` %in% input$reviewer |
+                                 abstract_table()$`Reviewer 3` %in% input$reviewer |
+                                 abstract_table()$`Reviewer 4` %in% input$reviewer |
+                                 abstract_table()$`Reviewer 5` %in% input$reviewer |
+                                 abstract_table()$`Reviewer 6` %in% input$reviewer |
+                                 abstract_table()$`Reviewer 7` %in% input$reviewer |
+                                 abstract_table()$`Reviewer 8` %in% input$reviewer |
+                                 abstract_table()$`Reviewer 9` %in% input$reviewer |
+                                 abstract_table()$`Reviewer 10` %in% input$reviewer |
+                                 abstract_table()$`Reviewer 11` %in% input$reviewer |
+                                 abstract_table()$`Reviewer 12` %in% input$reviewer |
                                  "All" %in% input$reviewer,
-                               union(input$cols_abstract,"Id")]
+                               union(input$cols_abstract, "Timestamp")]
       return(mydt)
     })
     
@@ -154,29 +166,26 @@ window_open_erum <- function(id, type) {
       if(length(s) == 0)
         return(h3("Select an abstract from the table to display the full info"))
       
-      search_id <- current_dt()[s, ]$Id
-      this_submission <- abstract_table()[abstract_table()$Id == search_id, ]
-      this_title <- this_submission$Title
-      this_id <- this_submission$Id
-      this_abstract <- this_submission$Description
-      this_track <- this_submission$Track
-      this_format <- this_submission$`Session format`
-      this_keywords <- this_submission$`Keywords (1-3)`
-      this_link <- this_submission$Link
-      this_firsttime <- this_submission$`First time presenting this?`
-      this_notes <- this_submission$`Speaker Notes`
-      
-      
+      search_id       <- current_dt()[s, ]$Timestamp
+      this_submission <- abstract_table()[abstract_table()$Timestamp == search_id, ]
+      this_id         <- this_submission$Timestamp
+      this_title      <- this_submission$`Title of talk / poster`
+      this_authors    <- this_submission$`List of authors`
+      this_abstract   <- this_submission$`Abstract (up to 300 words)`
+      this_format     <- this_submission$`What are you proposing to present?`
+      this_keywords   <- this_submission$`Keywords (up to 5, comma-separated)`
+      this_link       <- this_submission$`If you are presenting an R/Bioconductor package, please provide a URL to the package.`
+
       # form_id:
       s <- input$DT_abstracts_rows_selected
       if(length(s) == 0)
         return(NULL)
       
       this_submission <-  current_dt()[s, ]
-      this_id <- this_submission$Id
-      this_title <- this_submission$Title
+      this_author <- this_submission$`Name of presenter`
+      this_title  <- this_submission$`Title of talk / poster`
       
-      form_id <- gsub( "'", "", paste(this_id, this_title, sep = "|"))
+      form_id <- gsub( "'", "", paste(this_author, this_title, sep = "|"))
       
       message("type: ", input$submissionType)
       message("form_id(): ", form_id)
@@ -186,32 +195,24 @@ window_open_erum <- function(id, type) {
           h3("Title: "),
           tags$b(h2(this_title)),
           h4("Contribution identifier: "),
-          tags$p(paste(this_id, this_title, sep = "|")),
+          tags$p(paste(this_author, this_title, sep = "|")),
           h3("Abstract: "),
           tags$p(this_abstract),
-          h3("Track:"),
-          p(this_track),
           h3("Format:"),
           p(this_format),
           h3("Keywords:"),
           tags$b(this_keywords),
           h3("Link:"),
           p(this_link),
-          h3("First time presenting this?"), 
-          p(ifelse(this_firsttime=="Checked",yes = "Yes", no = "No")),
-          h3("Speaker notes:"),
-          p(this_notes),
-          
+
           shiny::actionButton(
             inputId = "launch_gform", label = "Open the Google Form to insert your evaluation", 
             icon = icon("database"), 
-            onclick =window_open_erum(form_id, input$submissionType),
+            onclick =window_open_eval\\(form_id, input$submissionType),
             class = "btn-success"
           )
         )
       )
-      
-    
       })
     
     output$DT_abstracts <- DT::renderDataTable({
